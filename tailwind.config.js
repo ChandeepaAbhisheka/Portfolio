6a7284fd02d8fab1ced8196d2e50639533ce3551
Chandeepa Abhisheka <chandeepaabhisheka455@gmail.com>
2025-10-20 14:29:41 +0530

    Upload the project to the repo

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: [
+    "variant",
+    [
+      "@media (prefers-color-scheme: dark) { &:not(.light *) }",
+      "&:is(.dark *)",
+    ],
+  ],
+};
